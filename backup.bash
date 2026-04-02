4048770b0193985dd43a7404f59ffdd3172b64d1
CobaltCactus <ao3.cobaltcactus@gmail.com>
2026-04-02 17:59:32 -0500

    Backup at Thu Apr  2 17:59:31 CDT 2026

--- backup.bash
+++ backup.bash
@@ -14,6 +14,24 @@ set -e;
 MOD_SUBDIRS=(
 "LMS_Option_1_a5aa97a9-4359-bbc1-6a3f-7b4beb433fe9"
 "LMS_option_1_nocheeks_50499f90-acc6-4996-1f63-73392807fdca"
+"LMS_option_2_bd288a42-f9ee-637c-f968-c696f878fff7"
+"LMS_option_2_nocheeks_a801e598-76d1-7a52-3b48-b9109c383b63"
+"LMS_option_3_d26e8065-8f7e-586f-614f-ce5875c6a582"
+"LMS_option_3_nocheeks_d202dfd5-d729-b774-f746-3614835157fc"
+"LMS_option_4_160b5ade-ef92-7454-b329-e32cf286cea2"
+"LMS_option_4_nocheeks_816ad0cf-0e8c-e75d-4eeb-84e81b295c85"
+"LMS_option_5_23d7d28a-4ee9-0b6c-70f1-f7cfec7a4797"
+"LMS_option_5_nocheeks_ffe5d9db-8e0b-0bb8-8b86-c663c600ba24"
+"LMS_option_6_039c586f-a236-d03c-dcd0-cd0ae42cc726"
+"LMS_option_6_noforehead_6ca55823-649e-e637-10ed-e4b069cd2892"
+"LMS_option_7_c01de848-77a7-8d0b-dcad-e95358a15a73"
+"LMS_option_7_nocheeks_00e11ce8-1a4f-4e85-cd1c-e6a27c54956d"
+"LMS_option_8_bd4d4f4e-ad86-735d-88f5-bc75a53046c3"
+"LMS_option_8_nocheeks_b2015a70-195e-d2a3-a6a6-334e9d5f08f9"
+"LMS_option_9_965e36c2-8bcd-b2f7-e397-2e7ae94d1d8e"
+"LMS_option_9_nocheeks_991bbe96-f067-a653-c638-6d100add74ac"
+"LMS_option_10_8d1b9261-021b-52e7-29c4-6c08f01c8d23"
+"LMS_option_10_nocheeks_bf7d9fcf-7c04-693d-048c-0f0d9c08d493"
 )
 
 # This is the MinGW64 path to a Steam install of the toolkit
